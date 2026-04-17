8dc0bcce4dcf9fef15aa071f17cc5a91ea79b7ed
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-17 13:11:38 -0300

    Create utils.py

---
+++ core/utils.py
@@ -0,0 +1,8 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def grade_answer(student, correct):
+    vectorizer = TfidfVectorizer()
+    tfidf = vectorizer.fit_transform([student, correct])
+    similarity = cosine_similarity(tfidf[0:1], tfidf[1:2])[0][0]
+    return round(similarity * 100, 2)
