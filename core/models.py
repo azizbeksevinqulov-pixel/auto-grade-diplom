0724d778d50391f10f8c9d441c7ef2e28975d7ac
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-17 14:44:47 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,8 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('teacher', 'O‘qituvchi'),
+        ('student', 'Talaba')
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+
+class Test(models.Model):
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class Question(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
     text = models.TextField()
-    correct_answer = models.CharField(max_length=200)
+    question_type = models.CharField(max_length=20)
+    correct_answer = models.TextField()
+    points = models.IntegerField(default=1)
+
 
 class Result(models.Model):
-    score = models.IntegerField()
+    score = models.FloatField()
