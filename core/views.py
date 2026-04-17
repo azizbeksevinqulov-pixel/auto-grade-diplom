608cbdd7a25025fb59b3d649344f48db1c20d853
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-17 13:19:48 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from .utils import grade_answer
 
 def test_page(request):
-    return HttpResponse("Test tizimi ishlayapti ✅")
+    student_answer = "Python bu dasturlash tili"
+    correct_answer = "Python dasturlash tili hisoblanadi"
+
+    score = grade_answer(student_answer, correct_answer)
+
+    return HttpResponse(f"Natija: {score}%")
