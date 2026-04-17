83562eb13b0274635c7cbac3a09d4a72fba9a3ba
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-17 11:39:31 -0300

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,27 @@
+import os
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = []
+
+ROOT_URLCONF = "config.urls"
+
+WSGI_APPLICATION = "config.wsgi"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
+    }
+}
+
+STATIC_URL = "/static/"
