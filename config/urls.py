7eac3d189b37a209d007ee5bbcb5e9594f606974
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-17 14:49:55 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
 from core.views import test_page
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", test_page),
 ]
