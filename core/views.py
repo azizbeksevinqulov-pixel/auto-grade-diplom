ff852934d9863dde673de0afe82a7d775a875b89
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-17 13:30:02 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -8,12 +8,25 @@ def test_page(request):
 
         score = grade_answer(student, correct)
 
-        return HttpResponse(f"Natija: {score}%")
+        return HttpResponse(f"""
+        <body style="background:#0f172a;color:white;font-family:sans-serif;text-align:center;padding-top:100px;">
+            <h1>Natija: {score}%</h1>
+            <a href="/" style="color:#38bdf8;">⬅️ Orqaga</a>
+        </body>
+        """)
 
     return HttpResponse("""
+    <body style="background:#0f172a;color:white;font-family:sans-serif;text-align:center;padding-top:100px;">
         <h2>Javob kiriting:</h2>
+
         <form method="post">
-            <input type="text" name="answer" />
-            <button type="submit">Tekshirish</button>
+            <input type="text" name="answer"
+                style="padding:10px;border-radius:8px;border:none;width:250px;" />
+            <br><br>
+            <button type="submit"
+                style="padding:10px 20px;border:none;border-radius:8px;background:#38bdf8;color:black;font-weight:bold;">
+                Tekshirish
+            </button>
         </form>
+    </body>
     """)
