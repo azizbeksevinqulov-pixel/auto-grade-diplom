626406d353bc3e25943798c1f441686376b7843a
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-18 12:11:46 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
 DEBUG = False
 
@@ -13,10 +13,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core" # 👈 MUHIM (siz qo‘shgan app)
+    "core",
 ]
 
-MIDDLEWARE = []
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
 
 ROOT_URLCONF = "config.urls"
 
@@ -29,4 +36,10 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "core.CustomUser"
+
 STATIC_URL = "/static/"
+
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/dashboard/"
+LOGOUT_REDIRECT_URL = "/login/"
