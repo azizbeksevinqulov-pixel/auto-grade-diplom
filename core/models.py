192179e672d18c2bd2ca26737d636fe0b4e5a66f
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-18 12:10:13 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -3,24 +3,29 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
-        ('teacher', 'O‘qituvchi'),
-        ('student', 'Talaba')
+        ('admin', 'Admin'),
+        ('teacher', 'Teacher'),
+        ('student', 'Student'),
     )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
 
 
 class Test(models.Model):
     title = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Question(models.Model):
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name="questions")
     text = models.TextField()
-    question_type = models.CharField(max_length=20)
     correct_answer = models.TextField()
-    points = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.text
 
 
 class Result(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
     score = models.FloatField()
