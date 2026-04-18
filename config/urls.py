f22636b1bcd96179d1e8a558cc7e215ed4ff8204
azizbeksevinqulov-pixel <azizbeksevinqulov@gmail.com>
2026-04-18 13:08:33 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,18 +3,27 @@ from django.urls import path
 from core.views import *
 
 urlpatterns = [
-    path("", login_view),  # 👈 ENG MUHIM (root qo‘shildi)
 
-    path("admin/", admin.site.urls),
+    # ===== ROOT =====
+    path("", login_view),
 
+    # ===== AUTH =====
     path("login/", login_view),
+    path("register/", register_view),
     path("logout/", logout_view),
-    path("dashboard/", dashboard),
 
+    # ===== ADMIN =====
+    path("admin/", admin.site.urls),
+    path("admin-panel/", admin_panel),
+    path("create-admin/", create_admin),
+
+    # ===== TEST SYSTEM =====
     path("create/", create_test),
     path("test/", take_test),
 
+    # ===== DATA =====
     path("users/", users_list),
     path("results/", results_list),
     path("stats/", stats_view),
+
 ]
